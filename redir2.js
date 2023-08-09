a08322d86320e63b0f3bc46a01aca3ba6322828e
glmiraizconnect <139340471+glmiraizconnect@users.noreply.github.com>
2023-08-09 20:21:11 +0900

    Update redir2.js

--- redir2.js
+++ redir2.js
@@ -57,7 +57,7 @@ var makeredirect = function () { return __awaiter(_this, void 0, void 0, functio
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
         })
-            .then(function (res) {window.open(res.json()[0])});
+            .then(function (res) {window.open(res.json()[0],'_blank')});
         return [2 /*return*/];
     });
     console.log('hey')
